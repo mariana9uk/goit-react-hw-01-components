8cc5d6b273d348c652217df8431ec40c60fdeefc
mariana9uk <maryanagolyuk@gmail.com>
2023-08-03 00:03:57 +0300

    json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import userData from '../'
+import { Profile } from "./profile/profile";
 export const App = () => {
   return (
     <div
@@ -10,6 +12,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
+<Profile/>
       React homework template
     </div>
   );
---
+++ src/components/profile/profile.js
@@ -0,0 +1,11 @@
+
+export function Profile (){
+return (
+    <img
+    src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+    alt="User avatar"
+    class="avatar"
+  />
+)
+
+}
